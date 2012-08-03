58bc2389d850aa2d43ef0ef5ddb2d1e042a7e8f6
Sean <seanvanpelt2@gmail.com>
2012-08-02 21:11:49 -0400

    Test
    Test

---
+++ gen/com/death_note_book/death/note/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.death_note_book.death.note;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/death_note_book/death/note/Rules_page.java
@@ -0,0 +1,33 @@
+package com.death_note_book.death.note;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.support.v4.app.NavUtils;
+
+public class Rules_page extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rules_page);
+        Typeface font1 = Typeface.createFromAsset(getAssets(), "Death_Note_Font.ttf"); 
+        Typeface font2 = Typeface.createFromAsset(getAssets(), "Ryuk_Font.ttf");
+        Typeface font3 = Typeface.createFromAsset(getAssets(), "Death_Note_Rules.ttf"); 
+        TextView view = (TextView) findViewById(R.id.rules); 
+        view.setTypeface(font2); 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    
+   
+    
+}
